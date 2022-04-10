0ad24f09611efc5973f37786218fd65e14e1fa79
WassupDaddy <clzlsaksldk@naver.com>
2022-04-10 13:14:03 +0900

    현재 3-4 등록수정조회API만들기중_각 클래스 만들고 오류예정

---
+++ src/main/java/com/achi/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.achi.book.springboot.service.posts;
+
+
+//import com.achi.book.springboot.Web.dto.PostsSaveRequestDto;
+//import com.achi.book.springboot.domain.posts.PostsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/test/java/com/achi/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.achi.book.springboot.domain.posts;
+
+import javafx.geometry.Pos;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After  //1
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given 어떠한 데이터가 준비되었다.
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()    //2
+                .title(title)
+                .content(content)
+                .author("clzlscjsrnr@gmail.com")
+                .build());
+
+        //when 어떠한 함수(메서드)를 실행한다.
+        List<Posts> postsList = postsRepository.findAll();  //3
+
+        //then 어떠한 결과가 나와야 한다!!!
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
