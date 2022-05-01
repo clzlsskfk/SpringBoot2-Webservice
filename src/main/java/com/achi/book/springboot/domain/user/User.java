1dbbf24a45702f27be6ff11efd7b713498758644
WassupDaddy <clzlsaksldk@naver.com>
2022-05-01 16:35:11 +0900

    5/1일(일요일) 5-3 연동중 오류 해결중
    SessionUser 까지 소스코드 작성완료 후 재커밋

---
+++ src/main/java/com/achi/book/springboot/domain/user/User.java
@@ -0,0 +1,59 @@
+package com.achi.book.springboot.domain.user;
+
+import com.achi.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Entity;
+
+//import javax.persistence.*;   //위의 임포트를 이처럼 *로 묶을 수도 있다. 하지만 필요한 것만 빼오는게 아니라서 무거워지겠지??(이건 내생각)
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+}
