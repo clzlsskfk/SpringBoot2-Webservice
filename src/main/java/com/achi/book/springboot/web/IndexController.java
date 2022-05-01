ff6b6f310c0bf23424787d3a7ca8e6d8bd689d44
WassupDaddy <clzlsaksldk@naver.com>
2022-05-01 12:20:32 +0900

    5/1일(일요일) 5-3 연동중 > Process 'command '/Library/Java/JavaVirtualMachines/jdk1.8.0_301.jdk/Contents/Home/bin/java'' finished with non-zero exit value 1 에러난 상태 커밋

--- src/main/java/com/achi/book/springboot/web/IndexController.java
+++ src/main/java/com/achi/book/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.achi.book.springboot.web;
 
 
+//import com.achi.book.springboot.config.auth.dto.SessionUser;
 import com.achi.book.springboot.service.posts.PostsService;
 import com.achi.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {  //  1
         model.addAttribute("posts", postsService.findAllDesc());
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        if (user != null) {
+//            model.addAttribute("userName", user.getName());
+//        }
 
         return "index";
 
